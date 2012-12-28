02a0a6481d2ff0f5e1acf8d1d8eabff47f0a2983
yokomizor <mail@rogerioyokomizo.com.br>
2012-12-28 17:21:36 +0000

    pep8

--- atados/atados/forms.py
+++ atados/atados/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.contrib.auth.forms import
-AuthenticationForm as ContribAuthenticationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm as ContribAuthenticationForm)
 from django.utils.translation import ugettext_lazy as _
 
 
