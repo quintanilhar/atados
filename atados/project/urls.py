071c4fbcba3517c9692712ec0f687b9c2811ac1e
Rogério Yokomizo <mail@rogerioyokomizo.com.br>
2012-12-29 10:48:02 -0200

    some layout adjustments

--- atados/project/urls.py
+++ atados/project/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template, redirect_to
 from django.utils.translation import ugettext_lazy as _
-from atados.atados.forms import AuthenticationForm
 
 urlpatterns = patterns(
     '',
