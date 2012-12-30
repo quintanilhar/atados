21e06c43cf893e4400e376bbd1fbe7a826205354
Rogério Yokomizo <mail@rogerioyokomizo.com.br>
2012-12-30 12:05:12 -0200

    layout adjustments

--- atados/project/urls.py
+++ atados/project/urls.py
@@ -7,5 +7,6 @@ from atados.project.views import ProjectCreateView
 urlpatterns = patterns(
     '',
 
+    url(r'^$', direct_to_template, {'template_name', 'atados/project/index.html'}, name='index'),
     url(_(r'^new$'), ProjectCreateView.as_view(), name='new'),
 )
