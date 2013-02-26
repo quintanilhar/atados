4329a632b326b3ac0fbe33db8af91bc6f20022c1
Rogério Yokomizo <mail@rogerioyokomizo.com.br>
2013-02-26 09:47:39 -0300

    added skills field

--- atados/project/templatetags/atados/project.py
+++ atados/project/templatetags/atados/project.py
@@ -1,8 +1,6 @@
 from django import template
-from django.conf import settings
 from django.template import Context
 from django.template.loader import get_template
-from django.forms.forms import BoundField
 from atados.project.models import Availability, WEEKDAYS, PERIODS
 import re
 
@@ -11,30 +9,34 @@ register = template.Library()
 
 @register.filter
 def as_availabilities_field(field):
-    if isinstance(field, BoundField):
-        availabilities = dict([(weekday_id, {'weekday_label': weekday_label, 'periods': {}})
-            for weekday_id, weekday_label in WEEKDAYS])
-        for availability in Availability.objects.all():
-            availabilities[availability.weekday]['periods'].update(
-                    {availability.period: availability.id})
+    availabilities = dict([(weekday_id, {'weekday_label': weekday_label, 'periods': {}})
+        for weekday_id, weekday_label in WEEKDAYS])
+    for availability in Availability.objects.all():
+        availabilities[availability.weekday]['periods'].update(
+                {availability.period: availability.id})
 
-        print availabilities
-        return get_template("atados/project/availabilities_field.html").render(
-            Context({
-                'field': field,
-                'availabilities': availabilities,
-                'periods': PERIODS,
-                'weekdays': WEEKDAYS,
-            })
-        )
-    else:
-        # Display the default
-        return settings.TEMPLATE_STRING_IF_INVALID
+    print availabilities
+    return get_template("atados/project/availabilities_field.html").render(
+        Context({
+            'field': field,
+            'availabilities': availabilities,
+            'periods': PERIODS,
+            'weekdays': WEEKDAYS,
+        })
+    )
 
 @register.filter
 def as_causes_field(field):
-    return settings.TEMPLATE_STRING_IF_INVALID
+    return get_template("atados/project/causes_field.html").render(
+        Context({
+            'field': field,
+        })
+    )
 
 @register.filter
 def as_skills_field(field):
-    return settings.TEMPLATE_STRING_IF_INVALID
+    return get_template("atados/project/skills_field.html").render(
+        Context({
+            'field': field,
+        })
+    )
